ed752c852bdb22d9bb8090c389b1ac68504bd451
sarezjay <sarezjay@gmail.com>
2021-05-06 12:43:14 -0700

    Initial Commit

---
+++ CoreMotionAlamoFire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreMotionAlamoFire
+//
+//  Created by IRON on 5/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
